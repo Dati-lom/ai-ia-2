d1c6dc31ca236a6a16625b54b79603b4a581a2e3
dati-lom <davit13.lomauridze@gmail.com>
2024-09-25 07:17:35 +0400

    YEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import FlashcardContainer from './Components/FlashcardContainer';
 import HomeScreen from './Components/HomePage';
 import Navbar from './Components/Navbar';
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
+import { BrowserRouter as  Router ,Route, Routes } from 'react-router-dom';
 import questionsEn from "./Data/QuestionsEn.json";
 import questionsKa from "./Data/QuestionsKa.json"; 
 function App() {
   const [language, setLanguage] = useState('en'); // Language state
   const [translations, setTranslations] = useState(questionsEn)
+  
   useEffect(()=>{
-    console.log("tra");
+    console.log("tra",Router);
     
     setTranslations(language === "en" ? questionsEn : questionsKa)
   },[language])
